d19773ec8bc2d02d6baa57fc3986eb16326a35d7
robertoallexandre <robertoallexandre@outlook.com>
2022-01-26 20:51:29 -0300

    Regras View

--- src/database/Pokemon.js
+++ src/database/Pokemon.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 module.exports = {
     async findByName(name){
-        const { data:Pokemon } = await axios.get('http://pokeapi.co/api/v2/pokemon/${name}');
+        const { data:Pokemon } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${name}`);
         return Pokemon;
     }
 }
